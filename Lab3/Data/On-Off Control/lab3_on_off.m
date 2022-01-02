0ce521a97d3df248871537517f54b4cb419defe3
huynhtancuong <huynhtancuong.hcm@gmail.com>
2022-01-02 13:59:15 +0300

    finish on-off control

---
+++ Lab3/Data/On-Off Control/lab3_on_off.m
@@ -0,0 +1,36 @@
+%define some constants
+Ke = 0.4870;
+Km = 0.4870;
+R = 8.8696;
+J = 0.0024;
+L = 0.0047;
+VOLTAGE = 8.05;
+DRAG = 0;
+TARGET_ANGLE = 580;
+
+%run the model
+model = sim("model_lab_3_on_off_control.slx");
+
+
+%put measured time and angle -> matrix
+exp_results = readmatrix('data_relay.txt');
+exp_times = exp_results(1:500, 1);
+exp_angles = exp_results(1:500, 2);
+
+model_times = model.coordinate.Time;
+model_angles = model.coordinate.Data;
+
+figure("Name", "On-off control", "WindowState", "maximized");
+hold on;
+%draw experiment line
+plot(exp_times, exp_angles);
+%draw model line
+plot(model_times, model_angles);
+%draw setpoint line
+setpoint_x = [0 4.5];
+setpoint_y = [580 580];
+line(setpoint_x,setpoint_y, 'Color', 'blue');
+%make it more beautiful
+legend({'Experiment', 'Model', 'Setpoint'}, 'Location','southeast');
+xlabel('Time, [sec]');
+ylabel('Angle, [deg]');
