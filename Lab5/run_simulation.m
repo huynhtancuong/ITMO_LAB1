afef90fc514806b06674e3f43f704f9b528f95e4
huynhtancuong <huynhtancuong.hcm@gmail.com>
2022-03-26 18:36:41 +0300

    added simulation model

---
+++ Lab5/run_simulation.m
@@ -0,0 +1,24 @@
+goal_x = 1
+goal_y = 1
+
+ks = 200
+kr = 200
+
+Ke = 0.4870;
+Km = Ke;
+R = 8.8696;
+J = 0.0024;
+Angle = 580;
+Tm = 0.0886;
+Ta = 0.0005299 * Tm;
+L = 0.0047;
+PI = 3.14159265359;
+Umax = 8.05;
+Umin = -8.05;
+Drag = 0.05;
+
+r = 0.025 % radius of wheel
+B = 0.15 % distance between wheels
+
+plot(out.X.Data, out.Y.Data)
+plot(out.plot)
